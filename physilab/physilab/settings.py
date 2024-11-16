4ced0025ec0567427e30f6da4e85f4e14b092c19
billysm23 <bllysm23@gmail.com>
2024-11-17 01:16:57 +0700

    Update settings.py

--- physilab/physilab/settings.py
+++ physilab/physilab/settings.py
@@ -100,8 +100,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+static_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
